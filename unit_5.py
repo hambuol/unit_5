23bcbcd01038b778760cfb49d91106756b0de5d9
hambuol <oliver.hamburger@ssfs.org>
2016-09-28 11:57:39 -0400

    started to add functions and loops

--- unit_5.py
+++ unit_5.py
@@ -1,1 +1,30 @@
 # written by oliver hamburger
+# this program is a number guessing game
+
+import random
+
+if input("would you like to guess my number? (y/n): ") == "y":
+    def get_random_number():
+        r_number = random.randrange(1, 100)
+        return r_number
+
+    def game_loop():
+        while True:
+            guess = input("pick a number between 1 and 100: ")
+
+
+
+
+else:
+    print("thanks for playing")
+
+
+
+
+
+
+
+
+
+
+
